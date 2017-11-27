99ee9988dfd15335ab4dc4ba54272efa307eefdc
jlyonas <jlyon@acumensolutions.com>
2017-11-27 12:32:10 -0500

    demo complete

--- rethinkdb-exp.js
+++ rethinkdb-exp.js
@@ -1,62 +1,58 @@
-const r = require("rethinkdbdash")({
+const rDB = require("rethinkdb");
+
+const config = {
 	host: "localhost",
 	port: 28015,
 	db: 'test'
-});
-
-const logData = data => {
-	console.log("data", JSON.stringify(data, null, 2));
 };
 
-const closeDbConnection = () => {
-	r.getPool().drain();
+const logData = (data, name) => {
+	console.log(name, JSON.stringify(data, null, 2));
 };
 
-const queryAllTvShows = () => {
-	return r.table('tv_shows').run()
+const connectRethinkDB = () => {
+	return rDB.connect(config)
 };
 
-const queryAllAuthors = () => {
-	return r.table('authors').run()
-	// .then((conn, cursor) => {
-	// 	cursor.toArray()
-	// 	.then(data => data);
-	// })
+const queryAllTvShows = conn => {
+	return rDB.table('tv_shows').run(conn);
 };
 
-const queryAuthorsByName = authorNames => {
-	return r.table('authors')
-	.filter(doc => {
-		return r.expr(authorNames).contains(doc("name"));
-	})
-	.run()
-	.then(data => {
-		console.log("inner authors by name", JSON.stringify(data, null, 2));
-		console.log("inner authors typeof", typeof data);
-		return data;
-	});
-	// .then(data => {
-	// 	console.log("authorsByName", JSON.stringify(data, null, 2));
-	// 	return data;
-	// })
+const queryAllAuthors = conn => {
+	return rDB.table('authors').run(conn);
 };
 
+const queryAuthorsByName = (authorNames, conn) => {
+	return rDB.table('authors')
+		.filter(author => {
+			return rDB.expr(authorNames).contains(author("name"));
+		})
+		.run(conn)
+};
 
-queryAllTvShows()
-.then(data => {
-	console.log("all tv shows", JSON.stringify(data, null, 2));
-	console.log("all tv shows typeof", typeof data);
+const closeDbConnection = conn => {
+	conn.close();
+};
+
+connectRethinkDB()
+.then(conn => {
+
+	queryAllTvShows(conn)
+		.then(cursor => cursor.toArray())
+		.then(data => logData(data, "all tv shows"))
+		.catch(console.error);
+
+	queryAllAuthors(conn)
+		.then(cursor => cursor.toArray())
+		.then(data => logData(data, "all authors"))
+		.catch(console.error);
+
+	queryAuthorsByName(["William Adama"], conn)
+		.then(cursor => cursor.toArray())
+		.then(data => logData(data, "authors by name"))
+		.catch(console.error);
+
+	return conn;
 })
-.then(queryAllAuthors)
-.then(data => {
-	console.log("all authors", JSON.stringify(data, null, 2));
-	console.log("all authors typeof", typeof data);
-})
-.then(queryAuthorsByName(["William Adama"]))
-.then(data => {
-	console.log("authors by name", JSON.stringify(data, null, 2));
-})
-.catch(err => {
-	console.error(err);
-})
-.finally(closeDbConnection);
+.then(closeDbConnection)
+.catch(console.error)
